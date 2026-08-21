a209cf9870844c5a65a6287807efa6b25db835bd
UNGJINYOO-creator <fineyou@gmail.com>
2026-08-21 20:38:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ import os
 
 app = FastAPI()
 
-# 1. 화면(정적 파일)을 서비스하겠다고 선언
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# 제미나이 클라이언트 초기화
 client = genai.Client()
 
-# 2. 주소창에 그냥 접속했을 때 index.html 화면을 보여줍니다.
 @app.get("/")
 def read_index():
     return FileResponse("static/index.html")
 
-# 3. 버튼을 눌렀을 때 제미나이가 문제를 만들어주는 기능
 @app.get("/generate-problem")
 def generate_problem():
     try:
